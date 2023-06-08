bdda99113cf825c1ade8b6f9ef095e3a73f1958c
Mitch Chen <30834970+scat2801@users.noreply.github.com>
2023-06-08 11:03:47 +0100

    Update dbnav.py
    Largest size file == raw image

--- dbnav.py
+++ dbnav.py
@@ -24,16 +24,33 @@ def folderNav(folder_path):
         sf_cleaned[i]   = subfolders[i].split(os.sep)[-1]
         file_list = os.listdir(subfolders[i])
         maxMaskFileSize = 0
+        fileSizeIter = 0
 
         file_name = list(file_list)
         col_two = ''
         col_three = ''
         
         for j in range(len(file_list)):
-            file_name [j] = file_list[j].split('.nii')[0]
-	
+            file_name[j] = file_list[j].split('.nii')[0]
+            fileSizeIter = os.path.getsize(subfolders[i] + os.sep + file_list[j])
+            if (fileSizeIter > maxMaskFileSize):
+                maxMaskFileSize = fileSizeIter
+            if 'nii' in file_list[j]:
+                if 'nii.gz' not in file_list[j]:
+                    print ("All files need to be in .nii.gz format, please check case", file_list[j])
+
         for j in range(len(file_list)):
             if 'nii' in file_list[j]:
+                if (maxMaskFileSize == os.path.getsize(subfolders[i] + os.sep + file_list[j])):
+                    col_two = folder_path + os.sep + sf_cleaned[i] + os.sep + file_list[j]
+                    j = len(file_list) + 1
+                else:
+                    col_three = folder_path + os.sep + sf_cleaned[i] + os.sep + file_list[j]
+                    row = [sf_cleaned[i], col_two, col_three]
+                    writer.writerow(row)
+                    col_three = ''
+
+                '''    
                 if file_name[j] == min(file_name, key=len):
                     if maxMaskFileSize >  os.path.getsize(subfolders[i] + os.sep + file_list[j]):
                         sys.exit('Inconsistent file size, largest file needs to be the data, check scan ID:' + sf_cleaned[i])
@@ -41,6 +58,7 @@ def folderNav(folder_path):
                         #col_two = './' + sf_cleaned[i] + os.sep + file_list[j]
                         col_two = folder_path + os.sep + sf_cleaned[i] + os.sep + file_list[j]
                         j = len(file_list) + 1
+                
 
         for j in range(len(file_list)):
             if 'nii' in file_list[j]:
@@ -51,6 +69,8 @@ def folderNav(folder_path):
                     row = [sf_cleaned[i], col_two, col_three]
                     writer.writerow(row)
                     col_three = ''
+                    '''
+
     f.close()
 
 if __name__ == "__main__":
